a3d080a97e1a46895db2be66262c3584bd9e89e3
Kairo Sumida <kairosumida@hotmail.com>
2018-01-25 14:41:01 -0200

    Criado o client da API

--- LojaApiClient/Program.cs
+++ LojaApiClient/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
 
 namespace LojaApiClient
 {
@@ -6,7 +9,18 @@ namespace LojaApiClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string conteudo;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://www.mocky.io/v2/52aaf5deee7ba8c70329fb7d");
+            request.Method = "GET";
+
+            WebResponse response = request.GetResponse();
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                conteudo = reader.ReadToEnd();
+            }
+            Console.Write(conteudo);
+            Console.Read();
         }
     }
 }
